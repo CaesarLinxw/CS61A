e1b59799c9150f50eb2c87af4f9f15818785e221
CaesarLinxw <xiaowei.lin1997@gmail.com>
2020-09-03 11:17:37 +0800

    hw03 completed

--- homework/hw03/hw03.py
+++ homework/hw03/hw03.py
@@ -219,5 +219,5 @@ def make_anonymous_factorial():
     >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
     True
     """
-    return 'YOUR_EXPRESSION_HERE'
+    return (lambda f: lambda n: f(f, n))(lambda f, n: n if n == 1 else mul(n, f(f, sub(n, 1)))) # from the solution
 
