37ac19f1001a9c4d45c844ce6c903d77a108f3ec
Xiaowei(Caesar) Lin <xiaowei.lin1997@gmail.com>
2020-09-14 15:02:42 +0800

    lab04 updated

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -49,6 +49,7 @@ def distance(city_a, city_b):
     5.0
     """
     "*** YOUR CODE HERE ***"
+    return sqrt((get_lat(city_a) - get_lat(city_b)) ** 2 + (get_lon(city_a) - get_lon(city_b)) ** 2)
 
 def closer_city(lat, lon, city_a, city_b):
     """
