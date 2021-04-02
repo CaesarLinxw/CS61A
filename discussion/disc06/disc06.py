c35e8db1e880459d3fe6014bb6bd63e8d11704b7
CaesarLinxw <xiaowei.lin1997@gmail.com>
2021-04-02 15:02:25 +0800

    disc06 updated

--- discussion/disc06/disc06.py
+++ discussion/disc06/disc06.py
@@ -60,6 +60,20 @@ def announce_losses(who, last_score=0):
         return announce_losses(who, score)
     return say
 
+def fox_says(start, middle, end, num):
+    """
+    >>> fox_says('wa', 'pa', 'pow', 3)
+    'wa-pa-pa-pa-pow'
+    >>> fox_says('fraka', 'kaka', 'kow', 4)
+    'fraka-kaka-kaka-kaka-kaka-kow'
+    """
+    def repeat(k):
+        if k == 1:
+            return middle
+        else:
+            return middle + '-' + repeat(k - 1)
+    return start + '-' + repeat(num) + '-' + end
+
 
 # Tree ADT
 
