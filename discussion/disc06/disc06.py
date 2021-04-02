ae75e0f4d21d0135abb1d8f92125fe5ac5c8b92e
CaesarLinxw <xiaowei.lin1997@gmail.com>
2021-04-02 14:51:37 +0800

    disc06 updated

--- discussion/disc06/disc06.py
+++ discussion/disc06/disc06.py
@@ -40,24 +40,24 @@ def memory_new(x, f):
     return g    
 
 def announce_losses(who, last_score=0):
-"""
->>> f = announce_losses(0)
->>> f1 = f(10, 0)
->>> f2 = f1(1, 10) # Player 0 loses points due to swine swap
-Oh no! Player 0 just lost 9 point(s).
->>> f3 = f2(7, 10)
->>> f4 = f3(7, 11) # Should not announce when player 0's score does not change
->>> f5 = f4(11, 12)
-"""
+    """
+    >>> f = announce_losses(0)
+    >>> f1 = f(10, 0)
+    >>> f2 = f1(1, 10) # Player 0 loses points due to swine swap
+    Oh no! Player 0 just lost 9 point(s).
+    >>> f3 = f2(7, 10)
+    >>> f4 = f3(7, 11) # Should not announce when player 0's score does not change
+    >>> f5 = f4(11, 12)
+    """
     assert who == 0 or who == 1, 'The who argument should indicate a player.'
     def say(score0, score1):
         if who == 0:
-            score = ________________________
+            score = score0
         elif who == 1:
-            score = ________________________
-        if ________________________:
-            ________________________________________________
-        return ________________________
+            score = score1
+        if score < last_score:
+            print('Oh no! Player %d just lost %d point(s).'%(who, last_score - score))
+        return announce_losses(who, score)
     return say
 
 
